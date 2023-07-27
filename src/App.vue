da848aad9d992a2be0791408c74a1dcb78576210
Md. Morshedul Arefin <arefin2k@gmail.com>
2023-07-27 06:33:17 +0600

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <script setup>
+import { ref, reactive } from 'vue'
 
+// You can show joke on button click in 2 ways here. Using variable or using object
+
+const joke = reactive({
+  content: 'Chuck Norris can do a wheelie on a unicycle.'
+})
+
+//const jokex = ref('Chuck Norris can do a wheelie on a unicycle.')
+
+function getAJoke() {
+    fetch('https://api.chucknorris.io/jokes/random')
+        .then(response => response.json())
+        .then(data => {
+            joke.content = data.value
+            // jokex.value = data.value
+        })
+    }
 </script>
 
 <template>
-    
+    <p>{{ joke.content }}</p>
+    <button @click="getAJoke()">Get a new joke</button>
 </template>
 
 <style scoped>
